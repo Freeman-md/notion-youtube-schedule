2e265fed10ff1083a3394d68fe085f949debdb8e
Freeman-md <freemansparda@gmail.com>
2021-07-04 12:51:12 +0100

    Get videos from database

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(123)
+const dotenv = require('dotenv').config()
+const {Client} = require('@notionhq/client')
+
+// Init client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN
+})
+
+const database_id = process.env.NOTION_DATABASE_ID
+
+const getVideos = async () => {
+  const payload = {
+    path: `databases/${database_id}/query`,
+    method: 'POST'
+  }
+  const {results} = await notion.request(payload)
+
+  console.log(results)
+}
+
+getVideos()
